1eeb052e8de0c521f2cb4076f0d0202b689db3c0
alecarraro <a.carraro@student.tudelft.nl>
2025-08-23 11:06:27 +0200

    fix small bugs

--- src/Algorithms/HLBS25/reach_homog.jl
+++ src/Algorithms/HLBS25/reach_homog.jl
@@ -1,13 +1,13 @@
-function reach_homog_HLBS25!(F::Vector{ReachSet{N,SparsePolynomialZonotope{N}}},
-                             Ω0::SparsePolynomialZonotope{N},
-                             Φ::AbstractMatrix,
+function reach_homog_HLBS25!(F::Vector{ReachSet{N,SparsePolynomialZonotope{N,VN,MN,MNI,VI}}},
+                             Ω0::SparsePolynomialZonotope{N,VN,MN,MNI,VI},
+                             Φ::MatrixZonotope{N,MN},
                              NSTEPS::Integer,
                              δ::N,
                              max_order::Integer,
                              taylor_order::Integer,
                              ::Val{false},
                              reduction_method::AbstractReductionMethod,
-                             Δt0::TimeInterval) where {N}
+                             Δt0::IA.Interval) where {N,VN,MN,MNI,VI}
     # initial reach set
     Δt = (zero(N) .. δ) + Δt0
     @inbounds F[1] = ReachSet(Ω0, Δt)
@@ -28,16 +28,16 @@ function reach_homog_HLBS25!(F::Vector{ReachSet{N,SparsePolynomialZonotope{N}}},
     return F
 end
 
-function reach_homog_HLBS25!(F::Vector{ReachSet{N,SparsePolynomialZonotope{N}}},
-                             Ω0::SparsePolynomialZonotope{N},
-                             Φ::AbstractMatrix,
+function reach_homog_HLBS25!(F::Vector{ReachSet{N,SparsePolynomialZonotope{N,VN,MN,MNI,VI}}},
+                             Ω0::SparsePolynomialZonotope{N,VN,MN,MNI,VI},
+                             Φ::MatrixZonotope{N,MN},
                              NSTEPS::Integer,
                              δ::N,
                              max_order::Integer,
                              taylor_order::Integer,
                              ::Val{true},
                              reduction_method::AbstractReductionMethod,
-                             Δt0::TimeInterval) where {N}
+                             Δt0::IA.Interval) where {N,VN,MN,MNI,VI}
     # initial reach set
     Δt = (zero(N) .. δ) + Δt0
     @inbounds F[1] = ReachSet(Ω0, Δt)
